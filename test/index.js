6f7ca7bc67d6bb6fd5d18040485a8e84d99971ed
Matt Simerson <matt@tnpi.net>
2026-03-10 10:27:17 -0700

    test: remove unnecessary done callbacks in synchronous tests

--- test/index.js
+++ test/index.js
@@ -1,51 +1,44 @@
 // node.js built-in modules
-const assert = require("assert");
+const assert = require('node:assert')
 
 // npm modules
-const fixtures = require("haraka-test-fixtures");
+const fixtures = require('haraka-test-fixtures')
 
 // start of tests
 //    assert: https://nodejs.org/api/assert.html
 //    mocha: http://mochajs.org
 
-beforeEach(function (done) {
-    this.plugin = new fixtures.plugin("recipient-routes-probe");
-    done(); // if a test hangs, assure you called done()
-});
+beforeEach(function () {
+    this.plugin = new fixtures.plugin('recipient-routes-probe')
+})
 
-describe("recipient-routes-probe", function () {
-    it("loads", function (done) {
-        assert.ok(this.plugin);
-        done();
-    });
-});
+describe('recipient-routes-probe', function () {
+    it('loads', function () {
+        assert.ok(this.plugin)
+    })
+})
 
-describe("load-recipient-routes-probe.ini", function () {
-    it("loads recipient-routes-probe.ini from config/recipient-routes-probe.ini", function (done) {
-        this.plugin.load_config();
-        assert.ok(this.plugin.cfg);
-        done();
-    });
+describe('load-recipient-routes-probe.ini', function () {
+    it('loads recipient-routes-probe.ini from config/recipient-routes-probe.ini', function () {
+        this.plugin.load_config()
+        assert.ok(this.plugin.cfg)
+    })
 
-    it("initializes enabled boolean", function (done) {
-        this.plugin.load_config();
-        assert.equal(this.plugin.cfg.cache.enabled, true, this.plugin.cfg);
-        done();
-    });
-});
+    it('initializes enabled boolean', function () {
+        this.plugin.load_config()
+        assert.equal(this.plugin.cfg.cache.enabled, true, this.plugin.cfg)
+    })
+})
 
-describe("uses text fixtures", function () {
-    it("sets up a connection", function (done) {
-        this.connection = fixtures.connection.createConnection({});
-        assert.ok(this.connection.server);
-        done();
-    });
+describe('uses text fixtures', function () {
+    it('sets up a connection', function () {
+        this.connection = fixtures.connection.createConnection({})
+        assert.ok(this.connection.server)
+    })
 
-    it("sets up a transaction", function (done) {
-        this.connection = fixtures.connection.createConnection({});
-        this.connection.transaction = fixtures.transaction.createTransaction({});
-        // console.log(this.connection.transaction)
-        assert.ok(this.connection.transaction.header);
-        done();
-    });
-});
+    it('sets up a transaction', function () {
+        this.connection = fixtures.connection.createConnection({})
+        this.connection.init_transaction()
+        assert.ok(this.connection.transaction.header)
+    })
+})
